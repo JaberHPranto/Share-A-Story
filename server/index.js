4bd3e95e69b81fa8e1bd6d90fd9cf9ceeb7fe442
JaberHPranto <jhpranto53@gmail.com>
2021-06-09 20:27:24 +0600

    setup done

---
+++ server/index.js
@@ -0,0 +1,23 @@
+// import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+
+// middleware
+app.use(express.json({limit:'30mb',extended:true}))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+
+// connect to mongodb
+const CONNECTION_URL = "mongodb+srv://pranto:pranto140@sharestory.tzce8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.port || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// returns a promise
+    .then(() => app.listen(PORT,()=>{console.log(`Server started on port ${PORT}`)}))
+    .catch((err) => console.log("Failed to start", err))
+    
+mongoose.set('useFindAndModify',true)
