9ed13676135cb5b50065f622cbe107eb5a85f86e
JaberHPranto <jhpranto53@gmail.com>
2021-06-17 05:09:33 +0600

    refactor

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,11 +1,12 @@
 import * as api from '../api';
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from '../constants/actionTypes';
 
 // Action creator
 
 export const getPosts = () => async(dispatch)=>{
     try {
         const {data} = await api.fetchPosts()
-        dispatch({ type: "FETCH_ALL", payload:data})
+        dispatch({ type: FETCH_ALL, payload:data})
     } catch (error) {
         console.log(error)
     } 
@@ -14,7 +15,7 @@ export const getPosts = () => async(dispatch)=>{
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createNewPost(post)
-        dispatch({type:"CREATE",payload:data})
+        dispatch({type:CREATE,payload:data})
     }catch(error) {
         console.log(error)
     } 
@@ -23,7 +24,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id,post) => async (dispatch) => {
     try {
         const { data } = await api.modifyPost(id, post)
-        dispatch({type:"UPDATE",payload:data})
+        dispatch({type:UPDATE,payload:data})
     }catch(error) {
         console.log(error)
     } 
@@ -33,7 +34,7 @@ export const updatePost = (id,post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.removePost(id)
-        dispatch({type:"DELETE",payload:id})
+        dispatch({type:DELETE,payload:id})
     }catch(error) {
         console.log(error)
     } 
@@ -42,7 +43,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id)
-        dispatch({type:"LIKE",payload:data})
+        dispatch({type:LIKE,payload:data})
 
     }catch(error) {
         console.log(error)
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 // import bodyParser from 'body-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
 import postsRoutes from './routes/posts.js'
 
 
 const app = express()
-
+dotenv.config()
 
 // middleware
 app.use(express.json({limit:'30mb',extended:true}))
@@ -17,10 +18,9 @@ app.use(cors())
 app.use('/posts',postsRoutes)
 
 // connect to mongodb
-const CONNECTION_URL = "mongodb+srv://pranto:pranto140@sharestory.tzce8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.port || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 // returns a promise
     .then(() => app.listen(PORT,()=>{console.log(`Server started on port ${PORT}`)}))
     .catch((err) => console.log("Failed to start", err))
